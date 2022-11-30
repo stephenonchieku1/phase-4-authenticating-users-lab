91e9ced71f63d4043ef9787afcbb6184f5a17c96
stephenonchieku1 <onchiekustephen901@gmail.com>
2022-11-30 22:43:14 +0300

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/create'
+  get 'sessions/destroy'
   resources :articles, only: [:index, :show]
+  get "/me", to: "user#show"
+  post "/login", to: "SessionController#create"
+  delete "/logout", to:"sessions#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
